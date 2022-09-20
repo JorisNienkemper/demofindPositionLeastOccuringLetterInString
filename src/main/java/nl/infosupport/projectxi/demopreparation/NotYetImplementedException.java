26290fa77db36ce6efbe9f5adbd4293ddddc9c6e
jorisn <jorisn@infosupport.com>
2022-09-20 18:38:28 +0200

    Add second test where the index of the least occurring character is B in ABAABA is 1

--- src/main/java/nl/infosupport/projectxi/demopreparation/NotYetImplementedException.java
+++ src/main/java/nl/infosupport/projectxi/demopreparation/NotYetImplementedException.java
@@ -4,4 +4,8 @@ public class NotYetImplementedException extends RuntimeException {
     public NotYetImplementedException(String message) {
         super(message);
     }
+
+    public NotYetImplementedException() {
+        super("Not yet implemented. Probalby you did find a new requirement. Contact the PO");
+    }
 }
--- src/test/java/nl/infosupport/projectxi/demopreparation/Demo01FindFirstLeastOccurringLetterTests.java
+++ src/test/java/nl/infosupport/projectxi/demopreparation/Demo01FindFirstLeastOccurringLetterTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 @DisplayName("""
     Vind de eerste positie van de  letter die het minst voorkomt in een gegeven String
@@ -24,13 +25,66 @@ public class Demo01FindFirstLeastOccurringLetterTests {
         assertThat(firstPosition).isEqualTo(0);
     }
 
+    @Test
+    @DisplayName("""
+    De eerste postitie in een String die alleen uit B's bestaat is 0
+    """)
+    void vindDeEersteB(){
+        //Arrange
+        String bees = "BBBB";
+        //Act
+        int firstPosition = findFirstPositionLeastOccurringLetter(bees);
+        //Assert
+        assertThat(firstPosition).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("""
+    De eerste postitie van de B in de String ABAABA is 1
+    """)
+    void vindDeEersteBInEenStringDieUitAasEnBeesBestaat(){
+        //Arrange
+        String bees = "ABAABA";
+        //Act
+        int firstPosition = findFirstPositionLeastOccurringLetter(bees);
+        //Assert
+        assertThat(firstPosition).isEqualTo(0);
+    }
     private int findFirstPositionLeastOccurringLetter(String sentence) {
-        char capitalA = 'A';
-        for(char charFromString : sentence.toCharArray()){
-            if(capitalA != charFromString){
-                throw new NotYetImplementedException("Not implemented yet. You probably found a new requirement. Chech out with the PO!");
+        char[] capitalsAandB = {'A','B'};
+        int [] captialCounter = new int[capitalsAandB.length];
+        for(int index = 0; index < capitalsAandB.length; index++){
+            for(char charFromString : sentence.toCharArray()){
+                if(capitalsAandB[index] == charFromString){
+                    captialCounter[index] += 1;
+                }
             }
         }
-        return 0;
+        int indexMaxValue = findIndexMostOccuringCapital(captialCounter);
+
+
+        return findFirstPosition(capitalsAandB[indexMaxValue],sentence);
+    }
+
+    private int findFirstPosition(char character,String sentence) {
+        char[] charactersInSentence = sentence.toCharArray();
+        for(int index = 0; index < charactersInSentence.length; index++){
+            if(character == charactersInSentence[index] ){
+                return index;
+            }
+        }
+        throw new NotYetImplementedException();
+    }
+
+    private int findIndexMostOccuringCapital(int[] captialCounter) {
+        int maxValue = captialCounter[0];
+        int indexMaxValue= 0;
+        for(int index = 1; index< captialCounter.length;index++ ){
+            if(captialCounter[index]> maxValue){
+                maxValue = captialCounter[index];
+                indexMaxValue = index;
+            }
+        }
+        return indexMaxValue;
     }
 }
