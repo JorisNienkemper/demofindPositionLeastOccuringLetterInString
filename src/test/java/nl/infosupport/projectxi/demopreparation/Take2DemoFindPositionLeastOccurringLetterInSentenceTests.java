30d7f575cd7ec05cd2bd4311a210047c8178a19f
jorisn <jorisn@infosupport.com>
2022-09-22 15:01:08 +0200

    Add 2 de test. Test slaagt

--- src/test/java/nl/infosupport/projectxi/demopreparation/Take2DemoFindPositionLeastOccurringLetterInSentenceTests.java
+++ src/test/java/nl/infosupport/projectxi/demopreparation/Take2DemoFindPositionLeastOccurringLetterInSentenceTests.java
@@ -13,20 +13,62 @@ public class Take2DemoFindPositionLeastOccurringLetterInSentenceTests {
         //1 Arrange
         String aas = "AAAA";
         //2 Act
-        int postieFirstA = findPositionFirstA(aas);
+        int postieFirstA = findFirstPositionOfTheLeastOccuringAorBees(aas);
 
         //3 Assert
         assertThat(postieFirstA).isEqualTo(0);
     }
 
-    private int findPositionFirstA(String sentence) {
-        char capitalA ='A';
+    @Test
+    @DisplayName("de verwachtepositie = 1 van de eerste B in de sentence = ABAABA alleen uit A's en B's bestaat")
+    void verwachtPositie1BijStringVanAlleenAasEnBees(){
+        //1 Arrange
+        String aas = "ABAABA";
+        //2 Act
+        int postieFirstB = findFirstPositionOfTheLeastOccuringAorBees(aas);
 
-        for(char characterInSentence: sentence.toCharArray()){
-            if(characterInSentence != capitalA){
-                throw new NotYetImplementedException("Over deze situatie heb ik nog niet nagedacht");
+        //3 Assert
+        assertThat(postieFirstB).isEqualTo(1);
+    }
+    private int findFirstPositionOfTheLeastOccuringAorBees(String sentence) {
+        char[] capitalsAorB ={'A','B'};
+        int [] counterCapitalAandB = new int[capitalsAorB.length];
+        for(int indexCapitalsAorB = 0; indexCapitalsAorB<capitalsAorB.length;indexCapitalsAorB+=1) {
+            for (char characterInSentence : sentence.toCharArray()) {
+                if (characterInSentence == capitalsAorB[indexCapitalsAorB]) {
+                    counterCapitalAandB[indexCapitalsAorB] += 1;
+                }
             }
         }
-        return 0;
+
+        int indexLowestFrequenceLetterCount = indexLowestFrequenceLetterCount(counterCapitalAandB);
+
+        char lowestFrequenceLetter = capitalsAorB[indexLowestFrequenceLetterCount];
+
+        int indexInSentence = findFirstB(lowestFrequenceLetter, sentence);
+
+        return indexInSentence;
+    }
+
+    private int findFirstB(char lowestFrequenceLetter, String sentence) {
+        char [] sentenceAsCharacters = sentence.toCharArray();
+        for(int indexCharInSentence =0;indexCharInSentence< sentenceAsCharacters.length;indexCharInSentence += 1){
+            if(lowestFrequenceLetter == sentenceAsCharacters[indexCharInSentence]){
+                return indexCharInSentence;
+            }
+        }
+        throw new NotYetImplementedException("Dat kan niet");
+    }
+
+    private int indexLowestFrequenceLetterCount(int[] counterCapitalAandB) {
+        int lowest = counterCapitalAandB[0];
+        int indexLowestValue = 0;
+        for(int indexCounter = 0;indexCounter< counterCapitalAandB.length;indexCounter+=1){
+            if(lowest > counterCapitalAandB[indexCounter]){
+                lowest = counterCapitalAandB[indexCounter];
+                indexLowestValue = indexCounter;
+            }
+        }
+        return indexLowestValue;
     }
 }
